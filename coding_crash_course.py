899a3f21d046c2f0c6f075f42e2cc2d61d8648c1
rebecca-riley <rriley1@uci.edu>
2018-09-05 17:43:10 -0700

    Added comments

--- coding_crash_course.py
+++ coding_crash_course.py
@@ -1,4 +1,5 @@
 def main():
+    # open files
     try:
         class_file = open('class_schedules.tsv','r')
         TA_file = open('TA_schedules.tsv','r')
@@ -6,13 +7,13 @@ def main():
         print('Couldn\'t open input file(s); ending program.')
         return
 
+    # data extraction
     class_data, TA_data = [], []
     file = [class_file,TA_file]
     data = [class_data,TA_data]
     for i in range(len(file)):
         for line in file[i]:
             data[i].append(line)
-
 
 if __name__ == '__main__':
     main()
