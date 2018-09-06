633803c009a3aae46510da47640d3e6b520811c2
rebecca-riley <rriley1@uci.edu>
2018-09-05 17:35:40 -0700

    Added try-catch block to file open

--- coding_crash_course.py
+++ coding_crash_course.py
@@ -1,6 +1,11 @@
 def main():
-    class_file = open('class_schedules.tsv','r')
-    TA_file = open('TA_schedules.tsv','r')
+    try:
+        class_file = open('class_schedules.tsv','r')
+        TA_file = open('TA_schedules.tsv','r')
+    except IOError:
+        print('Couldn\'t open input file(s); ending program.')
+        return
+
 
 if __name__ == '__main__':
     main()
