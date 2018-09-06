889341c1d076b18136d4ca5dc692bbee3e296735
rebecca-riley <rriley1@uci.edu>
2018-09-05 17:40:39 -0700

    Removed print statements for organization

--- coding_crash_course.py
+++ coding_crash_course.py
@@ -10,19 +10,8 @@ def main():
     TA_data = []
     for line in class_file:
         class_data.append(line)
-        print(line)
     for line in TA_file:
         TA_data.append(line)
-        print(line)
-
-    # print first, second, and last lines in data lists to make sure
-    # they look okay
-    print(class_data[0])
-    print(class_data[1])
-    print(class_data[len(class_data) - 1])
-    print(TA_data[0])
-    print(TA_data[1])
-    print(TA_data[len(TA_data) - 1])
 
 
 if __name__ == '__main__':
